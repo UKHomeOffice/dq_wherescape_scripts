9bb15476a7bc65a281715573c9e704c75ed243f3
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 16:56:13 +0000

    run_gp_load_nbtc_parameters.bat

---
+++ run_gp_load_nbtc_parameters.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem *************************************************************
+rem *** CALL THE CONFIG FILE
+rem *************************************************************
+
+call %~dp0\run_dq_db_load_config.bat
+
+rem *************************************************************
+rem *** RUN THE PYTHON SCRIPT
+rem *************************************************************
+
+"%PSEXEC_EXE%" /accepteula \\%IL2_HOST% -s cmd /c "%PYTHON_GPLOAD_LOAD_NBTC_PARAMETERS_BAT_FILE%" > %SCRIPTS_DIR%\run_gp_load_nbtc_parameters.log 2>&1
