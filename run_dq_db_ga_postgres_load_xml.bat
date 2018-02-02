e2f538e3988c5f697e6d0139afcf37f98b85da18
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 16:50:35 +0000

    run_dq_db_ga_postgres_load_xml.bat

---
+++ run_dq_db_ga_postgres_load_xml.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem *************************************************************
+rem *** CALL THE CONFIG FILE
+rem *************************************************************
+
+call %~dp0\run_dq_db_load_config.bat
+
+rem *************************************************************
+rem *** RUN THE PYTHON SCRIPT
+rem *************************************************************
+
+"%PSEXEC_EXE%" /accepteula \\%IL2_HOST% -w %DQ_DB_LOAD_ROOT_PATH%/scripts -s cmd /c "%PYTHON_GA_POSTGRES_LOAD_XML%"  > %SCRIPTS_DIR%\run_dq_db_ga_postgres_load_xml.log 2>&1
