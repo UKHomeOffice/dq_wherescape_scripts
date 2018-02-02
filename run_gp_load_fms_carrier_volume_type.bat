17dddbede37bdd9b360a9b68201586a57901cdd1
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 16:55:08 +0000

    run_gp_load_fms_carrier_volume_type.bat

---
+++ run_gp_load_fms_carrier_volume_type.bat
@@ -0,0 +1,13 @@
+REM @echo off
+
+rem *************************************************************
+rem *** CALL THE CONFIG FILE
+rem *************************************************************
+
+call %~dp0\run_dq_db_load_config.bat
+
+rem *************************************************************
+rem *** RUN THE PYTHON SCRIPT
+rem *************************************************************
+
+"%PSEXEC_EXE%" /accepteula \\%IL2_HOST% -s cmd /c "%PYTHON_GPLOAD_LOAD_FMS_CARRIER_VOLUME_TYPE_BAT_FILE%" > %SCRIPTS_DIR%\run_gp_load_carrier_volume_type.log 2>&1
