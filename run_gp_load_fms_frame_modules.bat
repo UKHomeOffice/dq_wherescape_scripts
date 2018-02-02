f68ae1afe2fd74991a58b8a12aa76b4ea2b27d1c
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 16:55:35 +0000

    run_gp_load_fms_frame_modules.bat

---
+++ run_gp_load_fms_frame_modules.bat
@@ -0,0 +1,13 @@
+REM @echo off
+
+rem *************************************************************
+rem *** CALL THE CONFIG FILE
+rem *************************************************************
+
+call %~dp0\run_dq_db_load_config.bat
+
+rem *************************************************************
+rem *** RUN THE PYTHON SCRIPT
+rem *************************************************************
+
+"%PSEXEC_EXE%" /accepteula \\%IL2_HOST% -s cmd /c "%PYTHON_GPLOAD_LOAD_FMS_FRAME_MODULES_BAT_FILE%" > %SCRIPTS_DIR%\run_gp_load_fms_frame_modules.log 2>&1
