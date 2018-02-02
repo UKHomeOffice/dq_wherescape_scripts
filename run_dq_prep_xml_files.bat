098d544c6ef1049a453447536b634e1a7c3b7a64
Chris Nesbitt-Smith <chris@cns.me.uk>
2018-02-02 16:54:51 +0000

    run_dq_prep_xml_files.bat

---
+++ run_dq_prep_xml_files.bat
@@ -0,0 +1,13 @@
+@echo off
+
+rem *************************************************************
+rem *** CALL THE CONFIG FILE
+rem *************************************************************
+
+call %~dp0\run_dq_db_load_config.bat
+
+rem *************************************************************
+rem *** RUN THE PYTHON SCRIPT
+rem *************************************************************
+
+"%PSEXEC_EXE%" /accepteula \\%IL2_HOST% -w %DQ_DB_LOAD_ROOT_PATH%/scripts -s cmd /c "%PYTHON_PREP_XML_FILES_SCRIPT%" > %SCRIPTS_DIR%\run_dq_prep_xml_files.log 2>&1
